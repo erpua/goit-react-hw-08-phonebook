149a7f4394c3e71f08b84d3733fd25ae59a7f389
Yevgeniy <zhe.tovtin@gmail.com>
2021-04-22 06:43:30 -0500

    Updates

---
+++ static/js/5.70572b30.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-08-phonebook"]=this["webpackJsonpgoit-react-hw-08-phonebook"]||[]).push([[5],{126:function(e,t,o){e.exports={wrapper:"HomeView_wrapper__3vtNd",title:"HomeView_title__1nL85"}},162:function(e,t,o){"use strict";o.r(t);o(0);var i=o(126),r=o.n(i),a=o(1);t.default=function(){return Object(a.jsx)("div",{className:r.a.wrapper,children:Object(a.jsx)("h1",{className:r.a.title,children:"Welcome ..."})})}}}]);
+//# sourceMappingURL=5.70572b30.chunk.js.map
